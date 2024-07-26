789b70dd2f1e29d954d3bdf2c994f7a88a8d0832
TofuAI <133058555+TofuAI@users.noreply.github.com>
2024-07-26 15:30:08 +0700

    Bài 5.1

--- Bai5_1.cpp
+++ Bai5_1.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main() {
-    int n = 10, k = 10;
-    int a[10] = {2, 3, 5, 9, 1, 7, 4, 6, 3, 2};
-    
+   int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (a[i] + a[j] == k) cout << a[i] << " " << a[j] << endl;
